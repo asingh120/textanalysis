5e863990374245c7aaa7e139fec8bfa5e4f6bcde
asingh120 <51126524+asingh120@users.noreply.github.com>
2019-09-13 10:58:48 -0400

    Add files via upload

---
+++ fleschdriver.py
@@ -0,0 +1,17 @@
+''' 
+Program : fleschdriver.py
+Author: Amit
+Date: 9/9/19
+
+Test driver for Flesch Index and Grade Level
+'''
+
+sentences = int(input("Sentences: "))
+words = int(input("Words: "))
+syllables = int(input("Syllables: "))
+
+index = 206.835 - 1.015 * (words / sentences) - 84.6 * (syllables / words)
+print("Flesch Index:", index)
+
+level = round(0.39 * (words / sentences) + 11.8 * (syllables / words) - 15.59)
+print("Grade Level:", level)
